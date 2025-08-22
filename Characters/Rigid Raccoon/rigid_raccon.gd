689abb3a9f5b3c8525523fa7f9c05a7e05fe13bd
henrylalonde <henryplalonde1@gmail.com>
2025-08-22 16:10:32 -0500

    rigid raccoon initial controls

--- Characters/Rigid Raccoon/rigid_raccon.gd
+++ Characters/Rigid Raccoon/rigid_raccon.gd
@@ -2,12 +2,13 @@ extends RigidBody2D
 
 @onready var animation_player = $AnimationPlayer
 
-# Called when the node enters the scene tree for the first time.
+var move_direction = Vector2(0.0, 0.0)
+
 func _ready() -> void:
 	lock_rotation = true
-	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	pass
+	move_direction.x = round(Input.get_axis("move_left", "move_right"))
+	move_direction.y = round(Input.get_axis("move_up", "move_down"))
+	apply_central_force(1000.0 * move_direction)
